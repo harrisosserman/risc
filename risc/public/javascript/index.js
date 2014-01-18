2254a54c16dedaa09317f4ea0c1dae070afc3135
Harris Osserman <harris.osserman@gmail.com>
2014-01-17 19:05:12 -0500

    Table for map is created

--- risc/public/javascript/index.js
+++ risc/public/javascript/index.js
@@ -1,24 +1,40 @@
-// $(function(ko) {
-//     this.playerName = ko.observable('playerName');
-//     this.enterGame = function() {
-//         console.log("click happened");
-//         console.log(this.playerName);
-//     };
-//     ko.applyBindings(new viewModel());
-//     console.log("loaded index.js");
-// }(window.ko));
-
-
-
 function viewModel() {
     var self = this;
     self.playerName = ko.observable();
     self.displayGameWaitingRoom = ko.observable(false);
     self.displayGameStart = ko.observable(true);
+    self.displayModal = ko.observable(true);
+    self.displayMap = ko.observable(true);
     self.enterGame = function() {
         self.displayGameWaitingRoom(true);
         self.displayGameStart(false);
+        // var data = {
+        //     "name": self.playerName
+        // };
+        // $.ajax('/game/', {
+        //             method: 'POST',
+        //             data: data
+        //         }).done(function(result) {
+        //             self.displayGameWaitingRoom(true);
+        //             self.displayGameStart(false);
+        //             // get names
+        //         });
+    };
+    self.startGame = function() {
+        self.displayModal(false);
+        self.displayMap(true);
     };
 }
 ko.applyBindings(new viewModel());
 
+(function() {
+    //function to build map out of table
+    var map = $("#map tbody");
+    for(var k=0; k<5; k++) {
+        map.append("<tr>");
+        for(var m=0; m<5; m++) {
+            map.append("<td>HI</td>");
+        }
+        map.append("</tr>");
+    }
+})();
