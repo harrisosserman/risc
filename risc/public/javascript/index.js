2aa6560835c462d8ba01817836473443e5fe64b1
Harris Osserman <harris.osserman@gmail.com>
2014-01-17 22:06:50 -0500

    started working on polling

--- risc/public/javascript/index.js
+++ risc/public/javascript/index.js
@@ -1,31 +1,47 @@
-function viewModel() {
-    var self = this;
-    self.playerName = ko.observable();
-    self.displayGameWaitingRoom = ko.observable(false);
-    self.displayGameStart = ko.observable(true);
-    self.displayModal = ko.observable(true);
-    self.displayMap = ko.observable(false);
-    self.enterGame = function() {
-        self.displayGameWaitingRoom(true);
-        self.displayGameStart(false);
-        // var data = {
-        //     "name": self.playerName
-        // };
-        // $.ajax('/game/', {
-        //             method: 'POST',
-        //             data: data
-        //         }).done(function(result) {
-        //             self.displayGameWaitingRoom(true);
-        //             self.displayGameStart(false);
-        //             // get names
-        //         });
-    };
-    self.startGame = function() {
-        self.displayModal(false);
-        self.displayMap(true);
-    };
-}
-ko.applyBindings(new viewModel());
+(function() {
+    function viewModel() {
+        var self = this;
+        self.playerName = ko.observable();
+        self.displayGameWaitingRoom = ko.observable(false);
+        self.displayGameStart = ko.observable(true);
+        self.displayModal = ko.observable(true);
+        self.displayMap = ko.observable(false);
+        self.playerList = ko.observableArray([]);
+        self.enterGame = function() {
+            self.displayGameWaitingRoom(true);
+            self.displayGameStart(false);
+            // var data = {
+            //     "name": self.playerName
+            // };
+            // $.ajax('/game/', {
+            //             method: 'POST',
+            //             data: data
+            //         }).done(function(result) {
+            //             self.displayGameWaitingRoom(true);
+            //             self.displayGameStart(false);
+            //             // get names
+            //         });
+            pollGameWaitingRoom(12345);      //pass in game id
+        };
+        self.startGame = function() {
+            self.displayModal(false);
+            self.displayMap(true);
+        };
+        self.pollGameWaitingRoom = function(gameId) {
+            while(true) {
+                $.ajax('/game/' + gameId, {
+                    method: 'GET'
+                }).done(function() {
+                    break;
+                }).fail(function(result) {
+                    //add each element from result into observable array
+                });
+                $.delay(5000);  //wait 5 seconds before polling again
+            }
+        };
+    }
+    ko.applyBindings(new viewModel());
+})();
 
 (function() {
     //function to build map out of table
