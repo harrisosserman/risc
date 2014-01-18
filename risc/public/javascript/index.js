eea76420080b654414bee74917b81575502ad860
Harris Osserman <harris.osserman@gmail.com>
2014-01-17 21:32:43 -0500

    map looks better

--- risc/public/javascript/index.js
+++ risc/public/javascript/index.js
@@ -4,7 +4,7 @@ function viewModel() {
     self.displayGameWaitingRoom = ko.observable(false);
     self.displayGameStart = ko.observable(true);
     self.displayModal = ko.observable(true);
-    self.displayMap = ko.observable(true);
+    self.displayMap = ko.observable(false);
     self.enterGame = function() {
         self.displayGameWaitingRoom(true);
         self.displayGameStart(false);
@@ -30,10 +30,12 @@ ko.applyBindings(new viewModel());
 (function() {
     //function to build map out of table
     var map = $("#map tbody");
+    var count = 1;
     for(var k=0; k<5; k++) {
         map.append("<tr>");
         for(var m=0; m<5; m++) {
-            map.append("<td>HI</td>");
+            map.append("<td>" + count + "</td>");
+            count++;
         }
         map.append("</tr>");
     }
