2fa9749a53cd4e5eeb13e8e39f2681a46f4a5577
Julian <jbt16@duke.edu>
2014-01-23 18:25:45 -0500

    Created a fake skeleton for Game and modified API for testing purposes

--- risc/app/controllers/API.java
+++ risc/app/controllers/API.java
@@ -7,12 +7,17 @@ import java.util.*;
 // import org.codehaus.jackson.JsonNode;
 import play.libs.Json;
 // import org.codehaus.jackson.node.ObjectNode;
+import models.Game;
 
 public class API extends Controller {
     public static Result createGame() {
         //fill in with game creation logic
         // ObjectNode result = Json.newObject();
         // result.put("gameId", "3938383");
+
+    	Game game = new Game();
+    	game.start();
+
         return ok("GAME IS CREATED");
     }
 }
--- risc/app/models/Game.java
+++ risc/app/models/Game.java
@@ -1,7 +1,20 @@
 package models;
 
 public class Game {
+
+	private static final String DEFAULT_GAME_ID = "12345";
+
+	public String myGameID;
+
 	public Game(){
-		System.out.println("Hello world");
+		this.myGameID = DEFAULT_GAME_ID;
+	}
+
+	public void addPlayer(String name){
+		System.out.println("add player with name: " + name);
+	}
+
+	public void start(){
+		System.out.println("Should start game: " + this.myGameID);
 	}
 }
