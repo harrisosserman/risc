f26fe0f5fdc36414a8a0e393270dda38336fb4a8
Harris Osserman <harris.osserman@gmail.com>
2014-01-27 23:04:20 -0500

    number of attacking troops is displayed

--- risc/public/javascript/board.js
+++ risc/public/javascript/board.js
@@ -110,7 +110,9 @@
             // var mapAppendArrow = $('#map').append("<span class='attackArrow glyphicon " + attackArrowPosition.class + "' style='top: " + attackArrowPosition.top + "px; left: " + attackArrowPosition.left + "px'>HELLO</span>");
             var preprendImageUrl = "https://cdn2.iconfinder.com/data/icons/ios-7-icons/50/";
             var appendImageUrl = "-128.png";
-            var mapAppendArrow = $("<img class='attackArrow' src='" + preprendImageUrl + attackArrowPosition.urlDirection + appendImageUrl + "'></img>").appendTo("body").css("top", attackArrowPosition.top + "px").css("left", attackArrowPosition.left + "px");
+            $("<img class='attackComponent' src='" + preprendImageUrl + attackArrowPosition.urlDirection + appendImageUrl + "'></img>").appendTo("body").css("top", attackArrowPosition.top + "px").css("left", attackArrowPosition.left + "px");
+            $("<h3 class='attackComponent'></h3>").appendTo("body").html(troopsAttacking).css("top", attackArrowPosition.textTop + "px").css("left", attackArrowPosition.textLeft + "px").css("color", "red");
+
         };
         self.findOrigin = function(destination, map) {
             //UPDATE FIND ORIGIN SO THAT IT USES ADJACENT TERRITORIES TO FIND ORIGIN
@@ -131,54 +133,72 @@
             var result = {
                 top: origin.top,
                 left: origin.left,
-                urlDirection: ' '
+                urlDirection: ' ',
+                textTop: origin.top,
+                textLeft: origin.left
             };
             if(origin.top > destination.top && origin.left === destination.left) {
                 //ARROW UP
                 result.top = result.top - upDownArrowPadding;
                 result.left = result.left + (widthOfTile / 4);
                 result.urlDirection = 'up';
+                result.textTop = result.top + upDownArrowPadding;
+                result.textLeft = result.left + widthOfTile/3;
             }
             if(origin.top < destination.top && origin.left === destination.left) {
                 //ARROW DOWN
                 result.top = result.top  + heightOfTile - upDownArrowPadding;
                 result.left = result.left + (widthOfTile / 4);
                 result.urlDirection = 'down';
+                result.textTop = result.top + heightOfTile/4;
+                result.textLeft = result.left + widthOfTile/4;
             }
             if(origin.top > destination.top && origin.left > destination.left) {
                 //ARROW UP AND LEFT
                 result.top = result.top - upDownArrowPadding;
                 result.left = result.left -  leftRightArrowPadding;
                 result.urlDirection = 'up_left';
+                result.textTop = result.top + upDownArrowPadding;
+                result.textLeft = result.left + leftRightArrowPadding;
             }
             if(origin.top < destination.top && origin.left < destination.left) {
                 //ARROW DOWN AND RIGHT
                 result.top = result.top  + heightOfTile - upDownArrowPadding;
                 result.left = result.left +  widthOfTile - leftRightArrowPadding;
                 result.urlDirection = 'down_right';
+                result.textTop = result.top + heightOfTile/4;
+                result.textLeft = result.left + widthOfTile/3;
             }
             if(origin.top > destination.top && origin.left < destination.left) {
                 //ARROW UP AND RIGHT
                 result.top = result.top - upDownArrowPadding;
                 result.left = result.left +  widthOfTile - leftRightArrowPadding;
                 result.urlDirection = 'up_right';
+                result.textTop = result.top + upDownArrowPadding;
+                result.textLeft = result.left + widthOfTile/3;
             }
             if(origin.top < destination.top && origin.left > destination.left) {
                 //ARROW DOWN AND LEFT
                 result.top = result.top  + heightOfTile - upDownArrowPadding;
                 result.left = result.left -  leftRightArrowPadding;
                 result.urlDirection = 'down_left';
+                result.textTop = result.top + heightOfTile/4;
+                result.textLeft = result.left + leftRightArrowPadding;
             }
             if(origin.top === destination.top && origin.left > destination.left) {
                 //ARROW LEFT
                 result.top = result.top  + heightOfTile/12;
                 result.left = result.left -  leftRightArrowPadding;
+                result.textTop = result.top  + heightOfTile/3;
+                result.textLeft = result.left + leftRightArrowPadding;
                 result.urlDirection = 'left';
             }
             if(origin.top === destination.top && origin.left < destination.left) {
                 //ARROW RIGHT
                 result.top = result.top  + heightOfTile/12;
                 result.left = result.left +  widthOfTile - leftRightArrowPadding;
+                result.textTop = result.top  + heightOfTile/3;
+                result.textLeft = result.left + widthOfTile/3;
                 result.urlDirection = 'right';
             }
             return result;
